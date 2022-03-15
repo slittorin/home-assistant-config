efdf89f8a1a3d0d0a49b33a13093e6446d5d4559
slittorin <sven.anders@littorin.nu>
2022-03-15 20:36:54 +0100

    Update backup scripts and sensors

--- scripts/copy_backup.sh
+++ scripts/copy_backup.sh
@@ -147,7 +147,7 @@ else
 fi
 
 # Log, and limit logfile to 1000 rows.
-RESULT="$(date +%Y%m%d_%H%M%S): No error. Removed files: ${FILES_REMOVED}, copied files ${FILES_COPIED}."
+RESULT="$(date +%Y%m%d_%H%M%S): No error. Removed files: ${FILES_REMOVED}. Copied files: ${FILES_COPIED}."
 echo "${RESULT}" >> ${logfile}
 tail -n1000 ${logfile} > ${logfile_tmp}
 rm ${logfile}
