5cd824c9a7c58fe3028b1577451a0084c8d3b03a
slittorin <sven.anders@littorin.nu>
2022-03-16 22:09:51 +0100

    Update of tariff sensor for cost reduction for solar production

--- scripts/grafana_github_push.sh
+++ scripts/grafana_github_push.sh
@@ -1,115 +1,87 @@
 #!/bin/bash
 #
 # Purpose:
-# Push configuration to github.
+# Push Grafana configuration on other server to github.
 #
 # Usage:
-# ./github_push.sh COMMENT
+# ./grafana_github_push.sh SERVER HOST COMMENT
 #
-# COMMENT is the comment to add to the push-commit.
-# If empty, the default comment will be: "Minor change."
+# SERVER    - Mandatory. Is the remote server in format: user@NAME/IP.
+#             Remember that the server must be able to run commands without password (such as key based login).
+# HOST      - The Grafana host/IP to connect to, including port.
+# COMMENT   - The comment to add to the push-commit.
+#             If empty, the default comment will be: "Minor change."
+#
+# Pre-req. is that the script for pushing Grafana JSON-files to Github exists on the other server as: /srv/grafana-git.sh
 
 # Load environment variables (mainly secrets).
 if [ -f "/config/.env" ]; then
     export $(cat "/config/.env" | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
 fi
 
-# Check input.
-if [ -z "$1" ]
-then
-    no_comment=1
-    comment="Minor change."
-else
-    no_comment=0
-    comment="$1"
-fi
-
 # Variables:
 config_dir="/config"
 base_dir="/config/scripts"
 log_dir="/config/logs"
-logfile="${log_dir}/github_push.log"
-logfile_tmp="${log_dir}/github_push.tmp"
+logfile="${log_dir}/grafana_github_push.log"
+logfile_tmp="${log_dir}/grafana_github_push.tmp"
 
-_initialize() {
-    touch "${logfile}"
+touch ${logfile}
 
-    echo ""
-    echo "----------------------------------------------------------------------------------------------------------------"
-    echo "$(date +%Y%m%d_%H%M%S): Starting Github push."
+# Check server.
+if [ -z "$1" ]; then
+    echo "ERROR. Server must be given."
+    echo "$(date +%Y%m%d_%H%M%S): ERROR. Server must be given." >> ${logfile}
+    exit 1
+else
+    SERVER="$1"
+fi
 
-    if [ ${no_comment} -eq 1 ] 
-    then
-        echo "$(date +%Y%m%d_%H%M%S): No input given, setting comment to default."
-    fi
-}
+# Check host.
+if [ -z "$2" ]; then
+    echo "ERROR. Host must be given."
+    echo "$(date +%Y%m%d_%H%M%S): ERROR. Grafana host must be given." >> ${logfile}
+    exit 1
+else
+    HOST="$2"
+fi
 
-_github_push() {
-    cd ${config_dir}
-    
-    exit_code=0
-    status_error=""
-    
-    # Add all in /config dir (according to .gitignore).
-    echo "$(date +%Y%m%d_%H%M%S): Added all in base directory"
-    git add .
-    
-    # We also add .storage, but allow .gitignore to only allow whitelisted files.
-    # This to be able to add Lovelace files managed by UI.
-    echo "$(date +%Y%m%d_%H%M%S): Added directory: .storage/"
-    git add .storage/
+# Check comment.
+if [ -z "$3" ]; then
+    NO_COMMENT=1
+    COMMENT="Minor change."
+else
+    NO_COMMENT=0
+    COMMENT="$3"
+fi
 
-    # Loop through all directories and add to git (according to .gitignore).
-    for dir in */ ; do
-        echo "$(date +%Y%m%d_%H%M%S): Added directory: ${dir}"
-        git add "${dir}"
-    done
+echo "$(date +%Y%m%d_%H%M%S): Starting Grafana Github push." >> ${logfile}
 
-    git status
-    git_exit_code=$?
-    if [ ${git_exit_code} -ne 0 ] 
-    then
-        exit_code=1
-        status_error+=" status (${git_exit_code})"
-    fi
+if [ ${NO_COMMENT} -eq 1 ]; then
+    echo "$(date +%Y%m%d_%H%M%S): No input given, setting comment to default." >> ${logfile}
+fi
 
-    git commit -m "${comment}"
-    git_exit_code=$?
-    if [ ${git_exit_code} -ne 0 ] 
-    then
-        exit_code=1
-        status_error+=" commit (${git_exit_code})"
-    fi
+# Initialize the remote script.
+RESULT=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "/srv/grafana-git.sh ${HOST} ${COMMENT}" 2>> ${logfile}`
+RESULT_CODE=$?
+if [ ${RESULT_CODE} -ne 0 ]; then
+    echo "ERROR. SSH command error. Exit code: ${RESULT_CODE}: ${RESULT}"
+    echo "$(date +%Y%m%d_%H%M%S): ERROR. SSH command error when removing triggering remote script. Exit code: ${RESULT_CODE}: ${RESULT}" >> ${logfile}
+    exit 1
+else
+    echo "$(date +%Y%m%d_%H%M%S): File ${FILE} remote script triggered." >> ${logfile}
+fi
 
-    git push origin master
-    git_exit_code=$?
-    if [ ${git_exit_code} -ne 0 ] 
-    then
-        exit_code=1
-        status_error+=" push (${git_exit_code})"
-    fi
+# Get the last row in the log.
+RESULT=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "tail -1 /srv/grafana-git.log" 2>> ${logfile}`
 
-    # Check if error occured with git commands.
-    if [ ${exit_code} -eq 0 ] 
-    then
-        status="No error."
-    else
-        status="Error in: git${status_error}."
-    fi
-}
-
-_finalize() {
-    echo "$(date +%Y%m%d_%H%M%S): ${status}"
-}
-
-# Main
-_initialize >> "${logfile}" 2>&1
-_github_push >> "${logfile}" 2>&1
-_finalize >> "${logfile}" 2>&1
-
-# Limit the number of rows in the logfile
+# Log, and limit logfile to 1000 rows.
+echo "${RESULT}" >> ${logfile}
 tail -n1000 ${logfile} > ${logfile_tmp}
 rm ${logfile}
 mv ${logfile_tmp} ${logfile}
 
-exit ${exit_code}
+# Show output
+echo "${RESULT}"
+
+exit 0
