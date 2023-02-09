b5edc10dcad0959615298d7e22a44fd1e4905021
slittorin <sven.anders@littorin.nu>
2023-02-09 20:16:43 +0100

    Updated server1 sensors, and added server1 config push to Github.

--- scripts/server1_github_push.sh
+++ scripts/server1_github_push.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 #
 # Purpose:
-# Push Grafana configuration on other server to github.
+# Push Server1 configuration on other server to github.
 #
 # Usage:
-# ./grafana_github_push.sh SERVER HOST COMMENT
+# ./server1_github_push.sh SERVER HOST COMMENT
 #
 # SERVER    - Mandatory. Is the remote server in format: user@NAME/IP.
 #             Remember that the server must be able to run commands without password (such as key based login).
-# HOST      - The Grafana host/IP to connect to, including port.
 # COMMENT   - The comment to add to the push-commit.
 #             If empty, the default comment will be: "Minor change."
 #
-# Pre-req. is that the script for pushing Grafana JSON-files to Github exists on the other server as: /srv/grafana-git.sh
+# Pre-req. is that the script for pushing Server1 JSON-files to Github exists on the other server as: /srv/server1-git.sh
 
 # Load environment variables (mainly secrets).
 if [ -f "/config/.env" ]; then
@@ -23,8 +22,8 @@ fi
 config_dir="/config"
 base_dir="/config/scripts"
 log_dir="/config/logs"
-logfile="${log_dir}/grafana_github_push.log"
-logfile_tmp="${log_dir}/grafana_github_push.tmp"
+logfile="${log_dir}/server1_github_push.log"
+logfile_tmp="${log_dir}/server1_github_push.tmp"
 rsa_file="/config/.ssh/id_rsa"
 known_hosts_file="/config/.ssh/known_hosts"
 
@@ -39,34 +38,24 @@ else
     SERVER="$1"
 fi
 
-# Check host.
-if [ -z "$2" ]; then
-    echo "ERROR. Host must be given."
-    echo "$(date +%Y%m%d_%H%M%S): ERROR. Grafana host must be given." >> ${logfile}
-    exit 1
-else
-    HOST="$2"
-fi
-
 # Check comment.
-if [ -z "$3" ]; then
+if [ -z "$2" ]; then
     NO_COMMENT=1
     COMMENT="Minor change."
 else
     NO_COMMENT=0
-    COMMENT="$3"
+    COMMENT="$2"
 fi
 
-echo "$(date +%Y%m%d_%H%M%S): Starting Grafana Github push." >> ${logfile}
+echo "$(date +%Y%m%d_%H%M%S): Starting Server1 Github push." >> ${logfile}
 echo "$(date +%Y%m%d_%H%M%S): Server: ${SERVER}" >> ${logfile}
-echo "$(date +%Y%m%d_%H%M%S): Host: ${HOST}" >> ${logfile}
 
 if [ ${NO_COMMENT} -eq 1 ]; then
     echo "$(date +%Y%m%d_%H%M%S): No comment given, setting comment to default." >> ${logfile}
 fi
 
 # Initialize the remote script.
-RESULT=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "sudo /srv/grafana-git.sh ${HOST} '${COMMENT}'" 2>> ${logfile}`
+RESULT=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "sudo /srv/server1-git.sh '${COMMENT}'" 2>> ${logfile}`
 RESULT_CODE=$?
 if [ ${RESULT_CODE} -ne 0 ]; then
     echo "ERROR. SSH command error. Exit code: ${RESULT_CODE}: ${RESULT}"
@@ -77,7 +66,7 @@ else
 fi
 
 # Get the last row in the log.
-RESULT=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "tail -1 /srv/log/grafana-git.log" 2>> ${logfile}`
+RESULT=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "tail -1 /srv/log/server1-git.log" 2>> ${logfile}`
 
 # Log, and limit logfile to 1000 rows.
 echo "${RESULT}" >> ${logfile}
