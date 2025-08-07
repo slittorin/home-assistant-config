f78f4777734c62b7c7a3a3aee1ac61b1a34cadf5
slittorin <sven.anders@littorin.nu>
2025-08-07 22:34:42 +0200

    Updated script for copy of backup-files to remote-server

--- scripts/copy_backup.sh
+++ scripts/copy_backup.sh
@@ -14,6 +14,12 @@
 #
 # Since shell commands by default do not have /backup mounted, we do this manually: https://community.home-assistant.io/t/tips-for-shell-command-and-auto-deleting-old-snapshots/207688
 # We could have utilized this approach, but chose manual mount: https://community.home-assistant.io/t/shell-command-backup-not-found/273055/8
+#
+# Updates:
+# 20250805: Added so that files with special characters (such as '&') can be managed, this as scp has problem with those.
+# 20250808: Changed so that files with special-characters, such as '&' in: Terminal_&_SSH_9.17.0_2025-06-25_13.57_50594240.tar, are better managed.
+#           I.e. workaround.
+#           Limitations: Do not manage filenames with newlines or similar, and do not check if sanitized file already exists.
 
 # Load environment variables (mainly secrets).
 if [ -f "/config/.env" ]; then
@@ -74,51 +80,67 @@ fi
 echo "$(date +%Y%m%d_%H%M%S): Local directory: ${backup_dir}" >> ${logfile}
 echo "$(date +%Y%m%d_%H%M%S): Remote directory: ${SERVER}:${REMOTEDIR}" >> ${logfile}
 
+# Since scp has problem with special-characters, and I could not isolate how to ensure that file-names where properly created on remote server,
+# I added code instead to change filenames with special-characters
+echo "$(date +%Y%m%d_%H%M%S): Check if filenames contains special-characters, if so replace with '_' instead." >> ${logfile}
+cd ${backup_dir} || exit 1
+LOCAL_FILES=`ls -1 *.tar 2> /dev/null`
+if [ ! -z "${LOCAL_FILES}" ]; then
+    for LOCAL_FILE in ${LOCAL_FILES}; do
+       SANITIZED_LOCAL_FILE=$(echo "${LOCAL_FILE}" | sed 's/[^A-Za-z0-9._-]/_/g')
+        if [ "${LOCAL_FILE}" != "${SANITIZED_LOCAL_FILE}" ]; then
+            mv "${LOCAL_FILE}" "${SANITIZED_LOCAL_FILE}"
+            echo "  $(date +%Y%m%d_%H%M%S): ${LOCAL_FILE}: Contained special characters and was sanitized." >> ${logfile}
+        fi
+    done
+fi
+
 # Since rsync does not exists in HA addon for shell, we need to make exact copy manually.
 # Check if all files in remote directory exists locally, if not, they are removed remote.
 # We check here if the file size is equal, but do not check hash of file.
+echo "$(date +%Y%m%d_%H%M%S): Check files in remote directory exists locally, if not, they are removed remote." >> ${logfile}
 FILES_REMOVED=0
-FILES=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "ls -1 ${REMOTEDIR} && ls -1 *.tar 2> /dev/null" 2>> ${logfile}`
-if [ ! -z "${FILES}" ]; then
-    for FILE in ${FILES}; do
-        FILE=`basename ${FILE}`
-        echo "${FILE}" >> ${logfile}
-        if [ ! -z ${FILE} ]; then
-            if [ ! -f "${backup_dir}/${FILE}" ]; then
-                RESULT=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "rm ${REMOTEDIR}/${FILE}" 2>> ${logfile}`
+REMOTE_FILES=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "ls -1 ${REMOTEDIR} && ls -1 *.tar 2> /dev/null" 2>> ${logfile}`
+if [ ! -z "${REMOTE_FILES}" ]; then
+    for REMOTE_FILE in ${REMOTE_FILES}; do
+        REMOTE_FILE=`basename ${REMOTE_FILE}`
+        if [ ! -z ${REMOTE_FILE} ]; then
+            if [ ! -f "${backup_dir}/${REMOTE_FILE}" ]; then
+                RESULT=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "rm ${REMOTEDIR}/${REMOTE_FILE}" 2>> ${logfile}`
                 RESULT_CODE=$?
                 if [ ${RESULT_CODE} -ne 0 ]; then
-                    echo "$(date +%Y%m%d_%H%M%S): For file ${FILE} does not exists locally, and is removed in remote directory." >> ${logfile}
                     echo "ERROR. SSH command error. Exit code: ${RESULT_CODE}: ${RESULT}"
-                    echo "$(date +%Y%m%d_%H%M%S): ERROR. SSH command error when removing remote file. Exit code: ${RESULT_CODE}: ${RESULT}" >> ${logfile}
+                    echo "  $(date +%Y%m%d_%H%M%S): ERROR. SSH command error when removing remote file: ${REMOTE_FILE}. Exit code: ${RESULT_CODE}: ${RESULT}" >> ${logfile}
+                    echo "  $(date +%Y%m%d_%H%M%S): Script ending prematurely." >> ${logfile}
                     exit 1
                 else
                     FILES_REMOVED=$((${FILES_REMOVED}+1))
-                    echo "$(date +%Y%m%d_%H%M%S): File ${FILE} does not exists locally, and is removed in remote directory." >> ${logfile}
+                    echo "  $(date +%Y%m%d_%H%M%S): ${REMOTE_FILE}: Does not exists locally, and is removed in remote directory." >> ${logfile}
                 fi
             else
-                SIZE_REMOTE=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "stat -c %s ${REMOTEDIR}/${FILE}" 2>> ${logfile}`
+                SIZE_REMOTE=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "stat -c %s ${REMOTEDIR}/${REMOTE_FILE}" 2>> ${logfile}`
                 RESULT_CODE=$?
                 if [ ${RESULT_CODE} -ne 0 ]; then
-                    echo "ERROR. SSH command error. Exit code: ${RESULT_CODE}: ${SIZE_REMOTE}"
-                    echo "$(date +%Y%m%d_%H%M%S): ERROR. SSH command error when getting hash for file. Exit code: ${RESULT_CODE}: ${RESULT}" >> ${logfile}
+                    echo "  ERROR. SSH command error. Exit code: ${RESULT_CODE}: ${SIZE_REMOTE}"
+                    echo "  $(date +%Y%m%d_%H%M%S): ERROR. SSH command error when getting size of remote file: ${REMOTE_FILE}. Exit code: ${RESULT_CODE}: ${RESULT}" >> ${logfile}
+                    echo "  $(date +%Y%m%d_%H%M%S): Script ending prematurely." >> ${logfile}
                     exit 1
                 fi
                 
-                SIZE=`stat -c %s ${backup_dir}/${FILE} 2>> ${logfile}`
+                SIZE=`stat -c %s ${backup_dir}/${REMOTE_FILE} 2>> ${logfile}`
                 if [ "${SIZE_REMOTE}" == "${SIZE}" ]; then
-                    echo "$(date +%Y%m%d_%H%M%S): Remote file ${FILE} exists locally and are equal in size, not required to be transfered." >> ${logfile}
+                    echo "  $(date +%Y%m%d_%H%M%S): ${REMOTE_FILE}: Remote file exists locally and are equal in size, not required to be transfered." >> ${logfile}
                 else
-                    RESULT=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "rm ${REMOTEDIR}//${FILE}" 2>> ${logfile}`
+                    RESULT=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "rm ${REMOTEDIR}/${REMOTE_FILE}" 2>> ${logfile}`
                     RESULT_CODE=$?
                     if [ ${RESULT_CODE} -ne 0 ]; then
-                        echo "$(date +%Y%m%d_%H%M%S): For remote file ${FILE} exists locally but files are not equal in size, therefore removed in remote directory." >> ${logfile}
-                        echo "ERROR. SSH command error. Exit code: ${RESULT_CODE}: ${RESULT}"
-                        echo "$(date +%Y%m%d_%H%M%S): ERROR. SSH command error when removing remote file. Exit code: ${RESULT_CODE}: ${RESULT}" >> ${logfile}
+                        echo "  ERROR. SSH command error. Exit code: ${RESULT_CODE}: ${RESULT}"
+                        echo "  $(date +%Y%m%d_%H%M%S): ERROR. SSH command error when removing remote file: ${REMOTE_FILE}. Exit code: ${RESULT_CODE}: ${RESULT}" >> ${logfile}
+                        echo "  $(date +%Y%m%d_%H%M%S): Script ending prematurely." >> ${logfile}
                         exit 1
                     else
                         FILES_REMOVED=$((${FILES_REMOVED}+1))
-                        echo "$(date +%Y%m%d_%H%M%S): Remote file ${FILE} exists locally but files are not equal in size, therefore removed in remote directory." >> ${logfile}
+                        echo "  $(date +%Y%m%d_%H%M%S): ${REMOTE_FILE}: Remote file exists locally but files are not equal in size, therefore removed in remote directory." >> ${logfile}
                     fi
                 fi
             fi
@@ -130,31 +152,36 @@ fi
 
 # Since rsync does not exists in HA addon for shell, we need to make exact copy manually.
 # Check if file exists remotely, if not, transfer.
-cd ${backup_dir}
+echo "$(date +%Y%m%d_%H%M%S): Check if files exists remotely, if not, transfer." >> ${logfile}
+cd ${backup_dir} || exit 1
 FILES_COPIED=0
-FILES=`ls -1 *.tar 2> /dev/null`
-if [ ! -z "${FILES}" ]; then
-    for FILE in ${FILES}; do
-        if [ ! -z ${FILE} ]; then
-            CHECK=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "ls -1 ${REMOTEDIR}/${FILE} 2> /dev/null" 2>> ${logfile}`
+LOCAL_FILES=`ls -1 *.tar 2> /dev/null`
+if [ ! -z "${LOCAL_FILES}" ]; then
+    for LOCAL_FILE in ${LOCAL_FILES}; do
+        if [ ! -z ${LOCAL_FILE} ]; then
+            CHECK=`ssh -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} ${SERVER} "ls -1 \"${REMOTEDIR}/${LOCAL_FILE}\" 2> /dev/null" 2>> ${logfile}`
             if [ -z ${CHECK} ]; then
-                echo "$(date +%Y%m%d_%H%M%S): File ${FILE} does not exists remotely, and is being transfered." >> ${logfile}
-                RESULT=`scp -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} -p ${backup_dir}/${FILE} ${SERVER}:${REMOTEDIR} 2>> ${logfile}`
+                echo "  $(date +%Y%m%d_%H%M%S): ${LOCAL_FILE}: Local file does not exists remotely, and is being transfered." >> ${logfile}
+                RESULT=`scp -o BatchMode=yes -o UserKnownHostsFile=${known_hosts_file} -i ${rsa_file} -p "${backup_dir}/${LOCAL_FILE}" "${SERVER}:${REMOTEDIR}/" 2>> ${logfile}`
                 RESULT_CODE=$?
                 if [ ${RESULT_CODE} -ne 0 ] 
                 then
-                    echo "ERROR. SSH command error when transferring file. Exit code: ${RESULT_CODE}: ${RESULT}"
-                    echo "$(date +%Y%m%d_%H%M%S): ERROR. SSH command error. Exit code: ${RESULT_CODE}: ${RESULT}" >> ${logfile}
+                    echo "  ERROR. SSH command error when transferring file: ${LOCAL_FILE}. Exit code: ${RESULT_CODE}: ${RESULT}"
+                    echo "  $(date +%Y%m%d_%H%M%S): ERROR. SSH command error. Exit code: ${RESULT_CODE}: ${RESULT}" >> ${logfile}
+                    echo "  $(date +%Y%m%d_%H%M%S): Script ending prematurely." >> ${logfile}
                     exit 1
                 else
                     FILES_COPIED=$((${FILES_COPIED}+1))
                 fi
+            else
+               echo "  $(date +%Y%m%d_%H%M%S): ${LOCAL_FILE}: Local file exists remotely, no need to transfer." >> ${logfile}
             fi
         fi
     done
 else
     echo "$(date +%Y%m%d_%H%M%S): No files in local directory." >> ${logfile}
 fi
+
 # Log, and limit logfile to 1000 rows.
 RESULT="$(date +%Y%m%d_%H%M%S): No error. Removed files: ${FILES_REMOVED}. Copied files: ${FILES_COPIED}."
 echo "${RESULT}" >> ${logfile}
--- scripts/systemair_mwrite.sh
+++ scripts/systemair_mwrite.sh
@@ -4,7 +4,8 @@
 # Write to Systemair Save Connect device.
 #
 # Usage:
-#   ./systemair_mwrite.sh IPADDRESS REGISTER VALUE
+#   ./systemair_mwrite.sh IPADDRESS/URL REGISTER VALUE
+#   URL is without http:
 # Example to set temperature setpoint to 17 degrees celcius.
 #   ./systemair_mwrite.sh 192.168.4.118 2000 170
 
@@ -23,8 +24,8 @@ else
 fi
 
 # Variables:
-arg_ip="$1"
-arg_register="$3"
+arg_url="$1"
+arg_register="$2"
 arg_value="$3"
 base_dir="/config/scripts"
 log_dir="/config/logs"
@@ -32,45 +33,51 @@ logfile="${log_dir}/systemair_mwrite.log"
 logfile_tmp="${log_dir}/systemair_mwrite.tmp"
 # Default exit code and status.
 exit_code=1
-status="Ending script."
+exit_output=""
 
 _initialize() {
     touch "${logfile}"
 
     echo ""
     echo "----------------------------------------------------------------------------------------------------------------"
-    echo "$(date +%Y%m%d_%H%M%S): Starting to Systemair Save Connect device"
+    echo "$(date +%Y%m%d_%H%M%S): Starting to write to Systemair Save Connect device"
 
     if [ ${no_variables} -eq 1 ] 
     then
         echo "$(date +%Y%m%d_%H%M%S): ERROR. Not enough arguments supplied."
+    else
+        echo "$(date +%Y%m%d_%H%M%S): URL: ${arg_url}"
+        echo "$(date +%Y%m%d_%H%M%S): Register: ${arg_register}"
+        echo "$(date +%Y%m%d_%H%M%S): Value: ${arg_value}"
     fi
 }
 
 _mwrite() {
     cd ${base_dir}
-    
-    status_error=""
 
-    url="http://" + arg_ip + "/mwrite?{%22" + arg_register + "%22:" + arg_value + "}"
-    curl_output=`curl ${url}`
+# curl -i 'http://192.168.4.118/mwrite?\{%222000%22:170\}' -H 'Accept: text/html,application/xhtml+xml' -H 'Accept-Language: en-US' -H 'Connection: close' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36' --insecure
+
+    curl_url="http://${arg_url}/mwrite?\{%22${arg_register}%22:${arg_value}\}"
+    curl_output=`curl -s -i ${curl_url} -H 'Accept: text/html,application/xhtml+xml' -H 'Accept-Language: en-US' -H 'Connection: close' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36' --insecure`
     curl_exit_code=$?
 
     if [ ${curl_exit_code} -ne 0 ] 
     then
         exit_code=1
-        status="Curl ended with error code: ${curl_exit_code}"
+        exit_output="ERROR. CURL ended with error code: ${curl_exit_code}."
+        echo "$(date +%Y%m%d_%H%M%S): ${exit_output}."
+        echo "$(date +%Y%m%d_%H%M%S): CURL output: ${curl_output}"
     else
+        # We take the last row as result.
         exit_code=0
-        status="No error."
-
-        # Give output from curl to Home Assistant.
-        echo ${curl_output}
+        exit_output=`echo "${curl_output}" | sed 's/\r$//' | tail -1`
+        echo "$(date +%Y%m%d_%H%M%S): No error from CURL."
+        echo "$(date +%Y%m%d_%H%M%S): CURL output: ${curl_output}"
     fi
 }
 
 _finalize() {
-    echo "$(date +%Y%m%d_%H%M%S): ${status}"
+    echo "$(date +%Y%m%d_%H%M%S): Ending script." >> "${logfile}" 2>&1
 }
 
 # Main
@@ -86,4 +93,6 @@ tail -n1000 ${logfile} > ${logfile_tmp}
 rm ${logfile}
 mv ${logfile_tmp} ${logfile}
 
+# Give output to Home Assistant.
+echo "${exit_output}"
 exit ${exit_code}
